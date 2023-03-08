a7e5e5fc1147625ccff68a5cfa84f1ecbd71d915
moti yosef <motiyosef@motis-MBP.lan>
2023-03-08 15:20:11 +0200

    stocks

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSession } from "next-auth/react";
 import { db } from "../firebaseConfig";
 import { doc, onSnapshot } from "firebase/firestore";
@@ -6245,6 +6245,7 @@ const Home = () => {
   const [pageNum, setPageNum] = useState(0);
   const [pagenumVisible, setPagenumVisible] = useState(true);
   const [info, setInfo] = useState();
+  const inputRef = useRef(null);
 
   //get the user Data from firebase
   useEffect(() => {
@@ -6336,31 +6337,34 @@ const Home = () => {
           <div className="stat-desc text-secondary">31 Stock Commented </div>
         </div>
         <div className="stat flex flex-col justify-between">
-          <div className="flex   relative  space-x-1 ">
-            <input
-              value={search}
-              type="text"
-              placeholder="Serach Stocks.."
-              className="input uppercase text-[#1b64a5] placeholder:normal-case text-xs md:text-base input-bordered bg-stone-200 h-12  w-full max-w-xs"
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            {search && (
-              <div className=" right-16 top-[25%] absolute  hidden  space-x-2 md:flex">
-                <a
-                  className={`  flex items-center h-6 text-gray-700 text-xs delay-300 tr300    `}
-                >
-                  {filteredItems.length > 0 && search.length > 0
-                    ? filteredItems.length + " results"
-                    : "No results"}
-                </a>
+          <div className="flex   space-x-1 ">
+            <div className="w-full relative max-w-xs">
+              <input
+                ref={inputRef}
+                value={search}
+                type="text"
+                placeholder="Serach Stocks.."
+                className="input uppercase text-[#1b64a5] placeholder:normal-case text-xs md:text-base input-bordered w-full max-w-xs bg-stone-200 h-12  "
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              {search && (
+                <div className=" absolute  top-[25%] right-[2%] hidden  md:flex">
+                  <a
+                    className={`  flex items-center h-6 text-gray-700 text-xs delay-300 tr300  w-20  `}
+                  >
+                    {filteredItems.length > 0 && search.length > 0
+                      ? filteredItems.length + " results"
+                      : "No results"}
+                  </a>
 
-                <MdCancel
-                  onClick={() => setSearch("")}
-                  className={`w-6 h-6 text-gray-700    hover:text-red-500 transition-all ease-in duration-300 `}
-                />
-              </div>
-            )}
-            <div className="dropdown static dropdown-end">
+                  <MdCancel
+                    onClick={() => setSearch("")}
+                    className={`w-6 h-6 text-gray-700    hover:text-red-500 transition-all ease-in duration-300 `}
+                  />
+                </div>
+              )}
+            </div>
+            <div className="dropdown static dropdown-end ">
               <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                 {session?.user?.image ? (
                   <div className="w-7 flex rounded-full">
@@ -6372,19 +6376,13 @@ const Home = () => {
               </label>
               <ul
                 tabIndex={0}
-                className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-stone-100 rounded-box w-52"
+                className="mt-3 p-2 shadow menu menu-compact dropdown-content text-gray-700 bg-stone-100 rounded-box w-52"
               >
                 <li>
                   <a className="justify-between">Profile</a>
                 </li>
                 <li>
                   <a href="#settings">Settings</a>
-                </li>
-                <li>
-                  <div className="">
-                    <a className="flex-1">Grid </a>
-                    <a href="">2</a>
-                  </div>
                 </li>
 
                 <li>
@@ -6420,10 +6418,190 @@ const Home = () => {
             </div>
           </div>
           <div className="flex justify-between h-12 items-center  ">
-            <CiGrid41 className="w-12 h-10 p-2  text-blue-500 rounded-xl hover:bg-stone-200  " />
-            <CiBookmark className="w-12 h-10 p-2  text-blue-500 rounded-xl hover:bg-stone-200  " />
-            <CiBellOn className="w-12 h-10 p-2  text-blue-500 rounded-xl hover:bg-stone-200  " />
-            <CiTimer className="w-12 h-10 p-2  text-blue-500 rounded-xl hover:bg-stone-200  " />
+            <div className="dropdown static ">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <CiTimer className="w-12 h-10 p-2  text-blue-500    " />
+              </label>
+              <ul
+                tabIndex={0}
+                className="mt-3 p-2 shadow menu menu-compact dropdown-content text-gray-700 bg-stone-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between">Profile</a>
+                </li>
+                <li>
+                  <a href="#settings">Settings</a>
+                </li>
+
+                <li>
+                  <div className="">
+                    <a className="flex-1"> Dark Mode </a>.
+                    <label className="swap swap-rotate">
+                      <input type="checkbox" />
+
+                      <svg
+                        className="swap-on fill-current w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                      </svg>
+
+                      <svg
+                        className="swap-off fill-current w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                      </svg>
+                    </label>
+                  </div>
+                </li>
+                <li>
+                  <a onClick={() => (session ? signOut() : signIn())}>
+                    {session ? `Logout` : "Login"}
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="dropdown static ">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <CiBellOn className="w-12 h-10 p-2  text-blue-500    " />
+              </label>
+              <ul
+                tabIndex={0}
+                className="mt-3 p-2 shadow menu menu-compact dropdown-content text-gray-700 bg-stone-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between">Profile</a>
+                </li>
+                <li>
+                  <a href="#settings">Settings</a>
+                </li>
+
+                <li>
+                  <div className="">
+                    <a className="flex-1"> Dark Mode </a>.
+                    <label className="swap swap-rotate">
+                      <input type="checkbox" />
+
+                      <svg
+                        className="swap-on fill-current w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                      </svg>
+
+                      <svg
+                        className="swap-off fill-current w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                      </svg>
+                    </label>
+                  </div>
+                </li>
+                <li>
+                  <a onClick={() => (session ? signOut() : signIn())}>
+                    {session ? `Logout` : "Login"}
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="dropdown static dropdown-end ">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <CiBookmark className="w-12 h-10 p-2  text-blue-500    " />
+              </label>
+              <ul
+                tabIndex={0}
+                className="mt-3 p-2 shadow menu menu-compact dropdown-content text-gray-700 bg-stone-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between">Profile</a>
+                </li>
+                <li>
+                  <a href="#settings">Settings</a>
+                </li>
+
+                <li>
+                  <div className="">
+                    <a className="flex-1"> Dark Mode </a>.
+                    <label className="swap swap-rotate">
+                      <input type="checkbox" />
+
+                      <svg
+                        className="swap-on fill-current w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                      </svg>
+
+                      <svg
+                        className="swap-off fill-current w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                      </svg>
+                    </label>
+                  </div>
+                </li>
+                <li>
+                  <a onClick={() => (session ? signOut() : signIn())}>
+                    {session ? `Logout` : "Login"}
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="dropdown static dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <CiGrid41 className="w-12 h-10 p-2  text-blue-500    " />
+              </label>
+              <ul
+                tabIndex={0}
+                className="mt-3 p-2 shadow menu menu-compact dropdown-content text-gray-700 bg-stone-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between">Profile</a>
+                </li>
+                <li>
+                  <a href="#settings">Settings</a>
+                </li>
+
+                <li>
+                  <div className="">
+                    <a className="flex-1"> Dark Mode </a>.
+                    <label className="swap swap-rotate">
+                      <input type="checkbox" />
+
+                      <svg
+                        className="swap-on fill-current w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+                      </svg>
+
+                      <svg
+                        className="swap-off fill-current w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+                      </svg>
+                    </label>
+                  </div>
+                </li>
+                <li>
+                  <a onClick={() => (session ? signOut() : signIn())}>
+                    {session ? `Logout` : "Login"}
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
@@ -6445,25 +6623,44 @@ const Home = () => {
           ))}
         </div>
       </div>
-      <div className=" min-h-screen space-y-4  bg-white backdrop-blur-lg items-center justify-center h py-2">
-        <div
-          className={` mt-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid flex-col  gap-2 `}
-        >
-          {filteredItems?.slice(pageNum, pageNum + 20).map((x, i) => (
-            <StockCard
-              x={x}
-              i={i}
-              list={info?.stock}
-              user={session?.user?.email}
-              info={info}
-              width={pageNum}
-              comments={info?.comments
-                ?.map((item) => item.stock == x && item.info)
-                .filter(Boolean)}
-            />
-          ))}
+      {filteredItems.length > 0 ? (
+        <div className=" min-h-screen space-y-4  bg-white backdrop-blur-lg items-center justify-center h py-2">
+          <div
+            className={` mt-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid flex-col  gap-2 `}
+          >
+            {filteredItems?.slice(pageNum, pageNum + 20).map((x, i) => (
+              <StockCard
+                x={x}
+                i={i}
+                list={info?.stock}
+                user={session?.user?.email}
+                info={info}
+                width={pageNum}
+                comments={info?.comments
+                  ?.map((item) => item.stock == x && item.info)
+                  .filter(Boolean)}
+              />
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className=" w-[100vw] h-[70vh] flex items-center justify-center">
+          <div className="card w-96 bg-primary text-primary-content">
+            <div className="card-body">
+              <h2 className="card-title">No results</h2>
+              <p>Write A New Search</p>
+              <div className="card-actions justify-end">
+                <button
+                  onClick={() => inputRef.current.focus()}
+                  className="btn"
+                >
+                  Search
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       <div
         className={`flex items-center   ${
           pagenumVisible ? "inset-[50%]  bottom-10  -translate-x-[50%] " : ""
