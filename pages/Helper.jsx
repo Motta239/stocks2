12a84329faf613aa302af6348c8768419aadffa9
Motta239 <105864476+Motta239@users.noreply.github.com>
2024-09-11 11:02:10 +0300

    Update Helper.jsx

--- pages/Helper.jsx
+++ pages/Helper.jsx
@@ -1,7 +1,7 @@
 const generateStockUrl = (x, timeFrame, toggole) => {
   const baseUrl =
     "https://charts2-node.finviz.com/chart.ashx?cs=l&t=%s&tf=%t&s=linear&ct=candle_stick&tm=d%o";
-  const tf = timeFrame ? "d" : "w";
+  const tf = timeFrame ? "w" : "d";
   const optionalParams = toggole
     ? "&o[0][ot]=sma&o[0][op]=50&o[0][oc]=FF8F33C6&o[1][ot]=sma&o[1][op]=200&o[1][oc]=DCB3326D&o[2][ot]=sma&o[2][op]=20&o[2][oc]=DC32B363&o[3][ot]=patterns&o[3][op]=&o[3][oc]=000"
     : "";
